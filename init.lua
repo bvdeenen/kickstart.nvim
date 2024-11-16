aac6aee61804950dddafe7ab61eff555116ac36d
Bart van Deenen <bartvandeenen@streammachine.io>
2024-11-16 15:19:24 +0100

    enabled vim-fugitive, autopairs and neo-tree

--- init.lua
+++ init.lua
@@ -325,6 +325,9 @@ require('lazy').setup({
     },
   },
   {
+    'tpope/vim-fugitive',
+  },
+  {
     lazy = false,
     'christoomey/vim-tmux-navigator',
     cmd = {
@@ -945,8 +948,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
