73f53f98ae73078e7cec6a44c2bf952c785d83b9
Bart van Deenen <bartvandeenen@streammachine.io>
2024-11-16 14:59:40 +0100

    tmux-navigator

--- init.lua
+++ init.lua
@@ -324,6 +324,22 @@ require('lazy').setup({
       },
     },
   },
+  {
+    lazy = false,
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    vim.keymap.set('n', '<c-h>', '<cmd> TmuxNavigateLeft <cr>', { desc = 'tmux left' }),
+    vim.keymap.set('n', '<c-j>', '<cmd> TmuxNavigateDown <cr>', { desc = 'tmux down' }),
+    vim.keymap.set('n', '<c-k>', '<cmd> TmuxNavigateUp <cr>', { desc = 'tmux up' }),
+    vim.keymap.set('n', '<c-l>', '<cmd> TmuxNavigateRight <cr>', { desc = 'tmux right' }),
+    vim.keymap.set('n', '<c-\\>', '<cmd> TmuxNavigatePrevious <cr>', { desc = 'tmux prev' }),
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
