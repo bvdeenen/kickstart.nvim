ff0f9537f2ae78c0737d3a7e3115dcc383741dea
Bart van Deenen <bartvandeenen@streammachine.io>
2024-12-04 18:39:26 +0100

    modularized

---
+++ lua/custom/plugins/cmake-tools.lua
@@ -0,0 +1,9 @@
+return {
+  'Civitasv/cmake-tools.nvim',
+  opts = {},
+  --config = function()
+  --require('cmake-tools').setup {
+  -- cmake_build_directory = 'build/${variant:buildType}',
+  --}
+  --end,
+}
